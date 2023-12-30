a77c6d045e293412a20a8ba4f58d094ba9bc4702
rttmoa <rttmoa@gmail.com>
2023-12-30 22:47:55 +0800

    feat：Hooks Component

---
+++ src/hooks/useErrorBoundary/index.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ErrorBoundaryContext = React.createContext(() => {});
+
+export const useErrorHandleing = () => React.useContext(ErrorBoundaryContext);
---
+++ src/hooks/useFetchReducer/index.ts
@@ -0,0 +1,39 @@
+import React from "react";
+
+type actionType = {
+  type: string;
+  data: any;
+  error: string;
+};
+// ? useFetchReducer:  https://codesandbox.io/s/elastic-framework-9wqd9?file=/src/App.js:140-188
+export default function useFetchReducer(initData = null) {
+  const initialState = {
+    status: "idle",
+    data: initData,
+    error: null
+  };
+  const fetchReducer = (currentState: any, action: actionType) => {
+    switch (action.type) {
+      case "FETCH":
+        return {
+          ...currentState,
+          status: "loading"
+        };
+      case "RESOLVE":
+        return {
+          status: "success",
+          data: action.data,
+          error: null
+        };
+      case "REJECT":
+        return {
+          data: null,
+          status: "failure",
+          error: action.error
+        };
+      default:
+        return currentState;
+    }
+  };
+  return React.useReducer(fetchReducer, initialState);
+}
