4b813f7333da0daadb012765b369dfdc9db1d195
rttmoa <rttmoa@gmail.com>
2023-12-30 16:13:02 +0800

    useLocalStorage

---
+++ src/hooks/useLocalStorage/index.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+function getStorageValue(key: string, defaultValue: any) {
+  if (typeof window !== "undefined") {
+    const saved = localStorage.getItem(key);
+    return saved !== null ? JSON.parse(saved) : defaultValue;
+  }
+}
+
+const useLocalStorage = (key: string, defaultValue: any) => {
+  const [value, setValue] = useState(() => getStorageValue(key, defaultValue));
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
+
+// const [value, setValue] = useLocalStorage()
