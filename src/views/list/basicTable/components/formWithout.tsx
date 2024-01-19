a77ac8089f3821c4aa3928c13efbbe116d472190
rttmoa <rttmoa@gmail.com>
2024-01-19 16:09:04 +0800

    fix(view): 🧩 update form content

---
+++ src/views/list/basicTable/components/formWithout.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable prettier/prettier */
+import React, { useState } from "react";
+import { Form, InputNumber } from "antd";
+
+type ValidateStatus = Parameters<typeof Form.Item>[0]["validateStatus"];
+
+const validatePrimeNumber = (number: number): { validateStatus: ValidateStatus; errorMsg: string | null } => {
+  if (number === 11) {
+    return {
+      validateStatus: "success",
+      errorMsg: null
+    };
+  }
+  return {
+    validateStatus: "error",
+    errorMsg: "The prime between 8 and 12 is 11!"
+  };
+};
+
+const formItemLayout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 12 }
+};
+
+const tips = "A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself.";
+
+const App: React.FC<{ value: string }> = ({ value: PropsValue }) => {
+  const [number, setNumber] = useState<{
+    value: number;
+    validateStatus?: ValidateStatus;
+    errorMsg?: string | null;
+  }>({ value: 11 });
+
+  const onNumberChange = (value: any) => {
+    setNumber({
+      ...validatePrimeNumber(value),
+      value
+    });
+  };
+
+  return (
+    <>
+      {PropsValue == "FormWithout" && (
+        <>
+          <Form style={{ maxWidth: 600 }}>
+            <Form.Item
+              {...formItemLayout}
+              label="Prime between 8 & 12"
+              validateStatus={number.validateStatus} // "" | "success" | "warning" | "error" | "validating" | undefined
+              help={number.errorMsg || tips} // React.ReactNode
+            >
+              <InputNumber min={8} max={12} value={number.value} onChange={onNumberChange} />
+            </Form.Item>
+          </Form>
+        </>
+      )}
+    </>
+  );
+};
+
+export default App;
--- src/views/list/basicTable/index.tsx
+++ src/views/list/basicTable/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React, { useEffect } from "react";
 import { Card, Typography, Button, Space, Select, Divider } from "antd";
-import FormCollectUser from "./components/fomCollectUser";
+import TableBasic from "./components/tableBasic";
 import FormBaisc from "./components/formBasic";
 import FromLayout from "./components/formLayout";
 import FormDisabled from "./components/formDisabled";
@@ -18,34 +18,29 @@ import { useLocation, useNavigate } from "react-router-dom";
 import SelectComp from "./selectComp";
 const { Text, Title } = Typography;
 
-// ? 选择表单类型
-// https://blog.csdn.net/Yin_yihui/article/details/129420273
-// https://ant.design/components/form-cn
-// https://procomponents.ant.design/components/login-form
-const BasicForm: React.FC = () => {
+// ? Table
+const BasicTable: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  let initValue = location.search ? location.search.split("=")[1] : "FromCollectUser";
+  let initValue = location.search ? location.search.split("=")[1] : "TableBasic";
   const [value, setValue] = React.useState<string>(initValue);
 
   // 要新添加 ? 自定义表单控件、多表单联动、内联登陆栏、普通登陆框、注册新用户、弹出层中的新建表单、时间类控件、自行处理表单数据
   const handleChange = (value: string) => {
     setValue(value);
-    navigate(`/form/basicForm?select=${value}`);
+    navigate(`/form/basicTable?select=${value}`);
   };
 
-  // ? 设计表单、？？
   return (
     <>
       <Card className="mb10">
         <Title level={4} className="mb15">
-          基础表单 - <Text>表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。</Text>
+          基础表格 - <Text>当有大量结构化的数据需要展现时 / 当需要对数据进行排序、搜索、分页、自定义操作等复杂行为时。</Text>
         </Title>
         <SelectComp handleChange={handleChange} initValue={initValue} />
       </Card>
       <Card className="mb10">
-        <hr />
-        <FormCollectUser value={value} />
+        <TableBasic isShow={value === "TableBasic"} />
         <FormBaisc value={value} />
         <FromLayout value={value} />
         <FormDisabled value={value} />
@@ -62,4 +57,4 @@ const BasicForm: React.FC = () => {
   );
 };
 
-export default BasicForm;
+export default BasicTable;
