db368ecd3272a5b5363a7be22ef7e7b062230523
rttmoa <rttmoa@gmail.com>
2024-01-25 12:06:57 +0800

    fix: 🧩 fix assets

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,9 @@ module.exports = {
       version: "detect"
     }
   },
-  /* Specify how to parse the syntax */
+  /* 指定如何解析语法 */
   parser: "@typescript-eslint/parser",
-  /* Configuration for parsing syntax with lower priority than 'parser' */
+  /* 优先级低于 parse 的语法解析配置 */
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
@@ -24,7 +24,7 @@ module.exports = {
     }
   },
   plugins: ["react", "@typescript-eslint", "prettier"],
-  /* Extend existing rules */
+  /* 继承某些已有的规则 */
   extends: [
     "eslint:recommended",
     "plugin:react/jsx-runtime",
@@ -32,27 +32,34 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended"
   ],
+  /*
+   * "off" 或 0    ==>  关闭规则
+   * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
+   * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
+   */
   rules: {
     // eslint (http://eslint.cn/docs/rules)
-    "no-var": "off", // Require using let or const instead of var
-    "no-multiple-empty-lines": ["error", { max: 5, maxEOF: 5 }], // Disallow multiple empty lines
-    "no-use-before-define": "off", // Disallow using functions/classes/variables before they are defined
-    "prefer-const": "off", // This rule is aimed at marking variables that are declared using let but never reassigned, and should be const instead
+    "no-var": "off", // 要求使用 let 或 const 而不是 var
+    "no-multiple-empty-lines": ["error", { max: 5, maxEOF: 5 }], // 不允许多个空行
+    "no-use-before-define": "off", // 禁止在 函数/类/变量 定义之前使用它们
+    "prefer-const": "off", // 此规则旨在标记使用 let 关键字声明但在初始分配后从未重新分配的变量，要求使用 const
+    "no-irregular-whitespace": "off", // 禁止不规则的空白
 
     // typeScript (https://typescript-eslint.io/rules)
-    "@typescript-eslint/no-unused-vars": "off", // Disallow unused variables
+    "@typescript-eslint/no-unused-vars": "off", // 禁止定义未使用的变量
     "@typescript-eslint/prefer-ts-expect-error": "off", // Disallow the use of @ts-ignore
-    "@typescript-eslint/ban-ts-comment": "off", // Disallow using @ts-<directive> comments or require descriptions after directives
-    "@typescript-eslint/no-inferrable-types": "off", // Allowing explicit types that can be easily inferred may add unnecessary verbosity
-    "@typescript-eslint/no-namespace": "off", // Disallow using custom TypeScript modules and namespaces
-    "@typescript-eslint/no-explicit-any": "off", // Disallow the use of the any type
-    "@typescript-eslint/ban-types": "off", // Disallow specific types
-    "@typescript-eslint/no-var-requires": "off", // Disallow the use of require statements in import statements
-    "@typescript-eslint/no-empty-function": "off", // Disallow empty functions
-    "@typescript-eslint/no-non-null-assertion": "off", // Disallow non-null assertion postfix operators (!)
+    "@typescript-eslint/ban-ts-comment": "off", // 禁止 @ts-<directive> 使用注释或要求在指令后进行描述
+    "@typescript-eslint/no-inferrable-types": "off", // 可以轻松推断的显式类型可能会增加不必要的冗长
+    "@typescript-eslint/no-namespace": "off", // 禁止使用自定义 TypeScript 模块和命名空间。
+    "@typescript-eslint/no-explicit-any": "off", // 禁止使用 any 类型
+    "@typescript-eslint/ban-types": "off", // 禁止使用特定类型
+    "@typescript-eslint/no-var-requires": "off", // 不允许在 import 语句中使用 require 语句
+    "@typescript-eslint/no-empty-function": "off",// 禁止空函数
+    "@typescript-eslint/no-non-null-assertion": "off", // 不允许使用后缀运算符的非空断言(!)
 
     // react (https://github.com/jsx-eslint/eslint-plugin-react)
-    "react-hooks/rules-of-hooks": "error", // Ensure hooks are called at the top level in a component or custom hook
-    "react-hooks/exhaustive-deps": "off" // The dependencies of useEffect and useCallback hooks do not need to be exhaustive
-  }
+    "react-hooks/rules-of-hooks": "error", // 确保在组件或自定义hook在顶层调用hook
+    "react-hooks/exhaustive-deps": "off", // useEffect和useCallback钩子的依赖关系不需要是全面的
+		"prettier/prettier": "off" // 关闭 prettier检测
+  },
 };
