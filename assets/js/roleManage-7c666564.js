0383af2d99b0964724e9cde8859af587e7f4ee2c
rttmoa <rttmoa@gmail.com>
2024-03-02 21:20:18 +0800

    Updates

--- assets/js/roleManage-7c666564.js
+++
@@ -1,1 +0,0 @@
-import{h as r,P as t}from"./index-f498645e.js";const o=()=>r.get(t+"/role/list"),a=s=>r.get(t+"/role/transfer/list");export{a,o as r};
---
+++ assets/js/roleManage-9a162c5b.js
@@ -0,0 +1,1 @@
+import{h as r,P as t}from"./index-5960db8a.js";const o=()=>r.get(t+"/role/list"),a=s=>r.get(t+"/role/transfer/list");export{a,o as r};
