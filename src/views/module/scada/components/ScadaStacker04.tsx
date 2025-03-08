9002b4baa3b83efa6ba839e4dfe42d678fb26558
rttmoa <rttmoa@gmail.com>
2025-03-08 12:30:39 +0800

    ·

---
+++ src/views/module/scada/components/ScadaStacker04.tsx
@@ -0,0 +1,70 @@
+import Shusongxian1 from '../icons/shusongxian1.png'
+import Huojia from '../icons/huojiaxinxi.png'
+
+const ScadaMapStarcker04 = () => {
+	const coverLines = [
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+	]
+	const shelves = [
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 300, y: 400 },
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+	]
+	return (
+		<div className="w-auto h-[100px] relative flex mt-[55px] ml-[10px]">
+			<div>
+				<div className="flex">
+					{coverLines.map((square, index) => (
+						<div key={index} className={`w-[30px] h-[30px]   ml-3`}>
+							<img src={Shusongxian1} alt="" />
+						</div>
+					))}
+				</div>
+				<div className="flex mt-5">
+					{coverLines.map((square, index) => (
+						<div key={index} className={`w-[30px] h-[30px]   ml-3`}>
+							<img src={Shusongxian1} alt="" />
+						</div>
+					))}
+				</div>
+			</div>
+			<div>
+				<div className="flex ml-4">
+					{shelves.map((square, index) => (
+						<div key={index} className={`w-[30px] h-[30px]  ml-[2px] -mt-[25px]`}>
+							<img src={Huojia} alt="" />
+						</div>
+					))}
+				</div>
+				<div className="mt-5 ml-6">堆垛机</div>
+				<div className="flex ml-4 mt-[45px]">
+					{shelves.map((square, index) => (
+						<div key={index} className={`w-[30px] h-[30px]   ml-[2px] -mt-[15px]`}>
+							<img src={Huojia} alt="" />
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ScadaMapStarcker04
--- src/views/module/scada/index.tsx
+++ src/views/module/scada/index.tsx
@@ -1,40 +1,126 @@
 import { Card } from 'antd'
 import ScadaImage from './Scada.png'
+import Shusongxian1 from './icons/shusongxian1.png'
+import Huojia from './icons/huojiaxinxi.png'
+
+import React from 'react'
 import './index.less'
+import ScadaMapStarcker01 from './components/ScadaStacker01'
+import ScadaMapStarcker03 from './components/ScadaStacker03'
+import ScadaMapStarcker02 from './components/ScadaStacker02'
+import ScadaMapStarcker04 from './components/ScadaStacker04'
 
 const Scada: React.FC = () => {
+	const obj1 = { uname: 1 }
 	return (
 		<div className="scada-container flex">
-			<Card className="sc-card relative w-full h-auto flex flex-col justify-center items-center overflow-hidden">
-				<img className="sc-img" src={ScadaImage} alt="" />
-				<div className="sc-1">123</div>
-				<ScadaMap />
+			<Card className="sc-card relative w-full h-screen">
+				<div
+					className="mx-auto w-[600px] h-[48px] bg-gradient-to-r from-orange-500 via-pink-500 to-blue-500 
+                flex items-center justify-center  text-white text-2xl font-bold uppercase shadow-lg z-50 rounded-2xl">
+					<span className="font-mono drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">Scada 平台</span>
+				</div>
+				{/* <img className="sc-img " src={ScadaImage} alt="" /> */}
+				{/* <div className="sc-1">123</div> */}
+				{/* <div className="w-[52px] h-[45px]  bg-cyan-200">sssss</div> */}
+
+				<div className="flex">
+					<div>
+						<ScadaMap1020 {...obj1} />
+						<ScadaMap1022 />
+						<ScadaMap1032 />
+						<ScadaMap1037 />
+					</div>
+					<div>
+						<ScadaMapStarcker01 />
+						<ScadaMapStarcker02 />
+						<ScadaMapStarcker03 />
+						<ScadaMapStarcker04 />
+					</div>
+				</div>
+				<div className="ml-[100px] mt-[20px]">
+					<div>每个站点说明</div>
+					<div>入库流程</div>
+					<div>出库流程</div>
+				</div>
 			</Card>
 		</div>
 	)
 }
-
-const ScadaMap = () => {
-	const blueSquares = [
+let dw = 30
+let dh = 30
+type ScadaParams1 = {
+	uname: number
+}
+const ScadaMap1020: React.FC<ScadaParams1> = ({ uname }) => {
+	const coverLines = [
 		{ x: 100, y: 200 },
 		{ x: 300, y: 400 },
 		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
 	]
 	return (
-		<div style={{ position: 'relative', width: '1000px', height: '600px', background: "url('/Scada.png') no-repeat", backgroundSize: 'cover' }}>
-			{blueSquares.map((square, index) => (
-				<div
-					key={index}
-					style={{
-						position: 'absolute',
-						left: `${square.x}px`,
-						top: `${square.y}px`,
-						width: '20px',
-						height: '20px',
-						backgroundColor: 'blue',
-						borderRadius: '5px',
-					}}
-				/>
+		<div className="w-[300px] relative flex mt-[100px] ml-[60px]">
+			{coverLines.map((square, index) => (
+				<div key={index} className={`w-[30px] h-[30px]   ml-3`}>
+					<img src={Shusongxian1} alt="" />
+				</div>
+			))}
+		</div>
+	)
+}
+
+const ScadaMap1022 = () => {
+	const coverLines = [
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
+	]
+	return (
+		<div className="w-[300px] relative flex mt-[25px] ml-[60px]">
+			{coverLines.map((square, index) => (
+				<div key={index} className={`w-[30px] h-[30px]   ml-3`}>
+					<img src={Shusongxian1} alt="" />
+				</div>
+			))}
+		</div>
+	)
+}
+const ScadaMap1032 = () => {
+	const coverLines = [
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
+	]
+	return (
+		<div className="w-[300px] relative flex mt-[225px] ml-[60px]">
+			{coverLines.map((square, index) => (
+				<div key={index} className={`w-[30px] h-[30px]   ml-3`}>
+					<img src={Shusongxian1} alt="" />
+				</div>
+			))}
+		</div>
+	)
+}
+const ScadaMap1037 = () => {
+	const coverLines = [
+		{ x: 100, y: 200 },
+		{ x: 300, y: 400 },
+		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
+		{ x: 500, y: 600 },
+	]
+	return (
+		<div className="w-[300px] relative flex mt-[25px] ml-[60px]">
+			{coverLines.map((square, index) => (
+				<div key={index} className={`w-[30px] h-[30px]   ml-3`}>
+					<img src={Shusongxian1} alt="" />
+				</div>
 			))}
 		</div>
 	)
